7783ec8780b0f9e83452e9b5e2596305a6b1b663
PhDoSoft <PhDoSoft@example.com>
2013-08-02 15:35:17 -0700

    Use embedded javadb because heroku doesn't has support for javadb

--- heroku.bat
+++ heroku.bat
@@ -1,12 +1,12 @@
 git config --global user.name "PhDoSoft"
 git config --global user.email PhDoSoft@example.com
-git add .
-git commit -m "initial checkin"
-heroku create --remote jruby-heroku brianna
+rem git add .
+rem git commit -m "initial checkin"
+rem heroku create --remote jruby-heroku brianna
 git push jruby-heroku master
 rem heroku ps:scale web=1
 rem heroku ps
 rem heroku open
-heroku logs
-heroku run rake db:migrate
-heroku run rake db:seed
+rem heroku logs --app brianna
+rem heroku db:migrate --app brianna
+rem heroku db:seed --app brianna
