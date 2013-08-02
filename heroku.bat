bbe6b88eda241d71a5fd31558e7691dcf5e63abb
PhDoSoft <PhDoSoft@example.com>
2013-08-02 15:10:32 -0700

    initial checkin

--- heroku.bat
+++ heroku.bat
@@ -2,7 +2,7 @@ git config --global user.name "PhDoSoft"
 git config --global user.email PhDoSoft@example.com
 git add .
 git commit -m "initial checkin"
-rem heroku create --remote jruby-heroku brianna
+heroku create --remote jruby-heroku brianna
 git push jruby-heroku master
 rem heroku ps:scale web=1
 rem heroku ps
