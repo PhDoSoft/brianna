0aadd35df08cc57cdd0ae0e5c1260cd62debeaf1
PhDoSoft <PhDoSoft@example.com>
2013-08-02 14:36:06 -0700

    Initial Checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brianna::Application.config.secret_token = '2419ddf0354d5e35a9523da56a899607938a8c8407c95f84357aea8641757518ddca9890b14f58f61fc5629c27a72dce1d22b3d1139f54b3aed2109d0c97c8ab'
